7706a6e0933ff6608b126e9166aa7bdaa78b78f9
Kumar MM <cryptokumaru@gmail.com>
2023-10-16 19:58:26 -0700

    param add

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,5 @@
+import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
-
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
     day: "numeric",
@@ -9,14 +9,16 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { cityName, country, emoji, date } = city;
+  const { cityName, emoji, date, id } = city;
   //   console.log(city);
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{formatDate(date)}</time>
-      <button className={styles.deleteBtn}>x</button>
+    <li>
+      <Link className={styles.cityItem} to={`${id}`}>
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}>x</button>
+      </Link>
     </li>
   );
 }
